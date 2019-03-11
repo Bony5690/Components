86295f3357b36938bfc2abb854280e808c3ffbf9
Jerry <jerrybony@yahoo.com>
2019-03-11 11:38:00 -0500

    styled input

--- components/App.js
+++ components/App.js
@@ -4,15 +4,32 @@ import DateTime from './Components/DatePicker';
 import Input from './Components/Input/Input';
 
 export default class App extends Component {
+    constructor(){
+      super()
+      this.state = {
+         value: 'Search for a property'
+      }
+    }
+
+    inputHandler = (e) => {
+      this.setState({value: e.target.value})
+    }
+
   render() {
     return (
       <View style={styles.container}>
-         <Input
-         imageStyle={{width: 20, height: 20}}
+      <View style={{marginTop: 50, marginBottom: 20, marginHorizontal: 20}}>
+      <Input
+         titleStyle={{color: '#8E8E93', marginBottom: 10, paddingLeft: 5, fontSize: 20, letterSpacing: 0.38, lineHeight: 24}}
+         style={{paddingLeft: 5, fontSize: 17, color: '#6DA3D3' }}
+         onChange={(value) => this.inputHandler(value)}
+         imageStyle={{width: 18, height: 18,}}
          source={require('./Components/Input/Assets/search.png')}
-        value='Search for a property'
+           value={this.state.value}
          placeholderTextColor="#FFFFFF"
          />
+      </View>
+        
          <DateTime/>
       </View>
     );
@@ -22,7 +39,7 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     backgroundColor: '#F5FCFF',
   }
 });
--- components/Components/Input/Input.js
+++ components/Components/Input/Input.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import { View,  TextInput, Image } from 'react-native';
+import { View,  TextInput, Image, Text, Button } from 'react-native';
 
 const Input = (props) => {
     return (
-        <View style={{ justifyContent: 'center', alignItems: 'center', margin: 20 }}>
-            <View style={{ flexDirection: 'row' }}>
+        <View style={{ justifyContent: 'center'}}>
+             <Text style={props.titleStyle}>Property</Text>
+            <View style={{ flexDirection: 'row', }}>
                 <Image
                     style={props.imageStyle}
                     source={props.source} />
-                <TextInput
-                        value={props.value}
+                    <TextInput
+                    style={props.style}
+                    onChange={props.onChange}
+                    value={props.value}
                     placeholderTextColor={props.placeholderTextColor}
                     placeholder={props.placeholder} />
+                    <View style={{textAlign:'center'}}>
+                    </View>
             </View>
+
         </View>
 
     )
